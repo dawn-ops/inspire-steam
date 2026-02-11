53ffc453155b73a16911e34a3ac1c40d6277c0d1
dawn-ops <ndinguripeter24@gmail.com>
2026-02-11 09:17:06 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,8 @@
+university = "jkuat"
+grade = "A"
+course = "data science"
+cluster_points = 74
+
+print(f"my university is {university}")
+print(f"my grade is {grade}")
+print(f"my course is {course}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# this is a comment
+print("hello from dawn")
+# city / town
+print("i am from nairobi")
+# my hobbies
+print("i like to play rugby,watch movies and coding")
+
+age=19
+height=1.25
+weight=95
+
+print(f"i am {age} years old")
+print(f"and i am also{height}meters")
+print(f"and weigh {weight} ")
